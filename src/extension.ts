21a753722f1bf241dd4474d098282445dcc4a34f
Plikss <plikssounaipas@gmail.com>
2026-05-06 16:24:11 +0200

    import miss match fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { FetchSourceMap, ProcessEdits, RojoProject } from './PathMaker';
+import { FetchSourceMap, ProcessEdits, RojoProject } from './PathResolver';
 
 let statusBarItem: vscode.StatusBarItem;
 let content: RojoProject | undefined;
